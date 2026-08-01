59645061ca9d3bde48e24ea42cad793738319ecb
mdwikurniawan976 <xyvnn478@gmail.com>
2026-08-02 00:07:06 +0700

    test

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,16 +1,16 @@
-import { createClient } from '@/utils/supabase.server';
-import { NextRequest, NextResponse } from 'next/server';
+// import { createClient } from '@/utils/supabase.server';
+// import { NextRequest, NextResponse } from 'next/server';
 
-export async function proxy(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-  if (pathname.startsWith('/admin')) {
-    const supabase = await createClient();
-    const { data: { session } } = await supabase.auth.getSession()
-    if (!session) {
-      return NextResponse.redirect(new URL('/login', request.url));
-    }
-    return NextResponse.next();
-  }
+// export async function proxy(request: NextRequest) {
+//   const { pathname } = request.nextUrl;
+//   if (pathname.startsWith('/admin')) {
+//     const supabase = await createClient();
+//     const { data: { session } } = await supabase.auth.getSession()
+//     if (!session) {
+//       return NextResponse.redirect(new URL('/login', request.url));
+//     }
+//     return NextResponse.next();
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
